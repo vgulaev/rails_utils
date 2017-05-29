f970ff31981be2d6d4bd0c978abc903b8481bf3b
vgulaev <vgulaev@yandex.ru>
2017-05-29 17:38:08 +0500

    Write optimization

--- export_table.rb
+++ export_table.rb
@@ -1,11 +1,13 @@
 require_relative 'rails_env'
+require "zlib"
 
 init_rails(:sql_logger_off => true)
 
 def export_portion(model, batch_size, options)
   objs = model.order(:id).where("id > #{options[:id]}").limit(batch_size)
   return false if objs.empty?
-  objs.each { |e| options[:batch] << e.attributes }
+  #objs.each { |e| options[:batch] << e.attributes.slice('id', 'body') }
+  objs.each { |e| options[:batch] << e.body }
   options[:id] = objs.last.id 
   true
 end
@@ -15,18 +17,25 @@ def export_whole_table(model, batch_size)
   i = 0
   while export_portion(model, batch_size, options) do
     i += batch_size
-    break if i > 10000 
+    yield options[:batch]
+    options[:batch] = []
+    break if i > 100000
     puts("Upload: #{i}")
   end
-  return options[:batch]
+  #return options[:batch]
 end
 
 model = MO
 
-batch = export_whole_table( model, 1000 )
+File.open("my.json", "wb") { |io| 
+  export_whole_table( model, 10000 ) do |data|
+    data = Zlib::Deflate.deflate(data.to_json, 9)
+    data_size = sprintf('%6d', data.size)
+    io.write(data_size)
+    io.write(data)
+  end
+}
 
-File.open("my.json", "w") { |io| io.write( batch.to_json ) }
-
+puts(File.size('my.json') / 1024 / 1024)
 puts('Ok')
-
-puts(dev?)
+# колотова
